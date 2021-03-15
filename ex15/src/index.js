90d638fb55f799328f1c356309b6b6a2f163b864
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-15 20:52:47 +0100

    ex15 added

---
+++ ex15/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function convertToInt(str) {
+  return parseInt(str, 16);
+}
+console.log(convertToInt("F1"));
+// Only change code above this line
+module.exports = convertToInt;
