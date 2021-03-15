b1183babaab58877589e34ae4b9f0979e8dc898e
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-15 12:02:22 +0100

    ex01 added

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+// Write code below this line
+    function reusableFunction(){
+        console.log("Hi Padawan!");
+    }
+// Write code above this line
+reusableFunction();
+module.exports = reusableFunction;
