8a4809fac6ad86ab2f962becfd6c25c07d7cda3b
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-15 19:12:08 +0100

    ex10 added

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function mathTest(x, y) {
+  // Only change code above this line
+  if (x < 0 || y < 0) {
+    // Only change code below this line
+    return;
+    // Only change code above this line
+  }
+  return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2));
+  // Only change code below this line
+}
+console.log(mathTest(2, 8));
+// Only change code above this line
+module.exports = mathTest;
