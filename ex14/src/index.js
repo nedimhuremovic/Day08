17767f0ea6fc65fabe08ef9d3fccf6729b1fe4e0
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-15 20:35:42 +0100

    ex14 added

---
+++ ex14/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function convertStrToInt(str) {
+  return parseInt(str);
+}
+
+console.log(convertStrToInt("24"));
+// Only change code above this line
+module.exports = convertStrToInt;
