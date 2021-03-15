3d1f740a23d45080c7adca32ddeaf21345916d3e
nedimhuremovic <nedimhuremovic@gmail.com>
2021-03-15 12:15:48 +0100

    ex01 modified and ex02 added

---
+++ ex02/src/index.js
@@ -0,0 +1,7 @@
+// Write code below this line
+function functionPrintInfo(name, age) {
+  console.log(name, age);
+}
+functionPrintInfo("Nedim",26);
+// Write code above this line
+module.exports = functionPrintInfo;
